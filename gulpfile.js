4fe98ffe76e18903ee2d53b4df8a4c57ee391663
Gleb Mikheev <mikheev@betaagency.ru>
2015-11-19 00:55:14 +0300

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,8 @@
 
 var gulp = require('gulp');
 var files = ['index.js', 'test/*.js', 'gulpfile.js'];
-var postcss = require('gulp-postcss');
-var cachebuster = require('./index.js');
-var rename = require('gulp-rename');
-
 
 gulp.task('test', function() {
-    /*
-    gulp.src('test/example.css')
-      .pipe(postcss([cachebuster({
-        imagesPath : './test/'
-      })]))
-      .pipe(rename('result.css'))
-      .pipe(gulp.dest('test/'));
-    */
     var mocha = require('gulp-mocha');
     return gulp.src('test/*.js', { read: false })
       .pipe(mocha());
