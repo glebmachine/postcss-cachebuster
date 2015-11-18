92de0abca76266f24ce3a08127a654534603307b
Gleb Mikheev <mikheev@betaagency.ru>
2015-11-19 01:05:42 +0300

    added .htc support + test

--- test/test.js
+++ test/test.js
@@ -15,23 +15,25 @@ var assert = function (input, output, opts, done) {
 };
 
 describe('postcss-cachebuster', function () {
-    var horseMtime = fs.statSync('./test/images/horse.jpg').mtime.getTime().toString(16);
+    var horseMtime = fs.statSync('./test/files/horse.jpg').mtime.getTime().toString(16);
+    var fontMtime = fs.statSync('./test/files/opensansbold.ttf').mtime.getTime().toString(16);
+    var htcMtime = fs.statSync('./test/files/backgroundsize.htc').mtime.getTime().toString(16);
 
     it('Process image, with relative path', function (done) {
-        assert('a { background-image : url("images/horse.jpg"); }', 
-               'a { background-image : url("images/horse.jpg?v'+horseMtime+'"); }', 
+        assert('a { background-image : url("files/horse.jpg"); }', 
+               'a { background-image : url("files/horse.jpg?v'+horseMtime+'"); }', 
                { cssPath : '/test/'}, done);
     });
 
     it('Process image, with absolute path', function (done) {
-        assert('a { background-image : url("/images/horse.jpg"); }', 
-               'a { background-image : url("/images/horse.jpg?v'+horseMtime+'"); }', 
+        assert('a { background-image : url("/files/horse.jpg"); }', 
+               'a { background-image : url("/files/horse.jpg?v'+horseMtime+'"); }', 
                { imagesPath : '/test/'}, done);
     });
 
     it('Process image, with spaces in name', function (done) {
-        assert('a { background-image : url("/images/horse with spaces.jpg"); }', 
-               'a { background-image : url("/images/horse%20with%20spaces.jpg?v'+horseMtime+'"); }', 
+        assert('a { background-image : url("/files/horse with spaces.jpg"); }', 
+               'a { background-image : url("/files/horse%20with%20spaces.jpg?v'+horseMtime+'"); }', 
                { imagesPath : '/test/'}, done);
     });
 
@@ -45,6 +47,18 @@ describe('postcss-cachebuster', function () {
         assert('a { background-image : url("there/is/no/image.jpg"); }', 
                'a { background-image : url("there/is/no/image.jpg"); }', 
                { imagesPath : '/test/'}, done);
+    });
+
+
+    it('Process font file', function (done) {
+        assert('a { src : url("files/opensansbold.ttf"); }', 
+               'a { src : url("files/opensansbold.ttf?v'+fontMtime+'"); }', 
+               { cssPath : '/test/'}, done);
+    });
+    it('Process .htc file', function (done) {
+        assert('a { behavior : url("files/backgroundsize.htc"); }', 
+               'a { behavior : url("files/backgroundsize.htc?v'+htcMtime+'"); }', 
+               { cssPath : '/test/'}, done);
     });
 
 });
